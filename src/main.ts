7e574f11df6faf986569f9d6be302b29a26fdc8a
brianqian <qian.brian@gmail.com>
2021-02-16 08:16:52 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify';
+
+const server = fastify({ logger: true });
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
